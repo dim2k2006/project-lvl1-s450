f456b722904616a154b950bd159cd46e54b7bd40
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-25 22:28:19 +0300

    Add "getUsername" function

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import {getUsername} from '../';
+
 console.log('Welcome to the Brain Games!');
+
+getUsername();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+/**
+ * Gets and prints user name
+ */
+const getUsername = () => {
+    const userName = readlineSync.question('May I have your name? ');
+
+    console.log(`Hi ${userName}!`);
+};
+
+export {getUsername};
