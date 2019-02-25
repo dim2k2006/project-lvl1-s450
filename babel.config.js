d5510b105edd02f6bcd4c08f17491388eb09ec44
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-25 22:21:10 +0300

    Update babel config. Add "targets" property.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
     api.cache(true);
 
-    const presets = ['@babel/env'];
+    const presets = [
+        ['@babel/preset-env',
+            {
+                targets: {
+                    node: 'current'
+                }
+            }
+        ]
+    ];
     const plugins = [];
 
     return {
