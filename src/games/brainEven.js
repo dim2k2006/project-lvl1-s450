ef1c149d4ef167fbe5aadc5a57a6346c5f1f2739
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-26 17:35:43 +0300

    Update "brainEven" function

--- src/games/brainEven.js
+++ src/games/brainEven.js
@@ -18,13 +18,13 @@ const brainEven = () => {
   const launch = (attempt) => {
     if (attempt === successAttemptsRequired) return say(`Congratulations, ${userName}!`);
 
-    const randomNumber = getRandomInteger(1, 100);
+    const question = getRandomInteger(1, 100);
 
-    say(`Question: ${randomNumber}`);
+    say(`Question: ${question}`);
 
     const answer = readlineSync.question('Your answer: ');
 
-    const result = (randomNumber % 2 === 0)
+    const result = (question % 2 === 0)
       ? 'yes'
       : 'no';
 
