174de3d5be8d9245017f21a2d7bcd50849aec842
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-26 14:54:25 +0300

    Fix indent size

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import readlineSync from 'readline-sync';
  * Gets and prints user name
  */
 const getUsername = () => {
-    const userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
 
-    console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 /**
  * Launches game
  */
 const launchGame = () => {
-    console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
 
-    getUsername();
+  getUsername();
 };
 
 export default launchGame;
