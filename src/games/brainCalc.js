c2e08774e0272008df834cb512ec13031eb5fea6
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-27 15:13:26 +0300

    Update "brainCalc" function

--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -2,8 +2,6 @@ import { Parser } from 'expr-eval';
 import engine from '../engine';
 import { getRandomExpression } from '../utils';
 
-const math = new Parser();
-
 /**
  * Brain calc game
  */
@@ -11,6 +9,7 @@ const brainCalc = () => {
   const description = 'What is the result of the expression?';
   const getData = () => {
     const question = getRandomExpression();
+    const math = new Parser();
     const expression = math.parse(question);
     const result = String(expression.evaluate());
 
