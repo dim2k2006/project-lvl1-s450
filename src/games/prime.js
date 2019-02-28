2195aa8d21ad4b06cec87acdeeeee88611155960
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-02-28 17:47:18 +0300

    Update "prime" game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,8 @@
 import engine from '../engine';
 import { getRandomInteger } from '../utils';
 
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
 /**
  * Checks if number is prime
  * @param {Number} number
@@ -15,18 +17,19 @@ const isPrime = (number) => {
 };
 
 /**
+ * Retrieves data needed for game
+ * @returns {Object}
+ */
+const getData = () => {
+  const question = getRandomInteger(1, 100);
+  const answer = isPrime(question);
+
+  return { question, answer };
+};
+
+/**
  * Brain prime game
  */
-const prime = () => {
-  const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const getData = () => {
-    const question = getRandomInteger(1, 100);
-    const result = isPrime(question);
-
-    return { question, result };
-  };
-
-  engine(description, getData);
-};
+const prime = () => engine(description, getData);
 
 export default prime;
